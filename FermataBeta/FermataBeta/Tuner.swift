98faaf215f1579b4fe6ae268f21483152a345cc8
youngliu323 <youngliu323@gmail.com>
2016-02-15 18:53:21 -0600

    Finished tuner

--- FermataBeta/FermataBeta/Tuner.swift
+++ FermataBeta/FermataBeta/Tuner.swift
@@ -12,9 +12,14 @@ import Foundation
 import AVFoundation
 
 class Tuner: UIViewController {
-   
+
+    
+/***************************************************
+    PLAYS A PITCH FOR THE USER
+***************************************************/
     var audioPlayer = AVAudioPlayer()
     var count = 0
+    var beingPlayed = 0
     
     var noteList = [String]()
     var noteNames = [String]()
@@ -28,6 +33,37 @@ class Tuner: UIViewController {
         }
         print(count)
         Note.text = noteNames[count]
+        
+        //automatically changes the sound upon the user changing it if it's being played
+        //pretty much the play button function
+        if beingPlayed == 1 {
+                playNote()
+        }
+        
+    }
+    
+    func playNote()
+    {
+        //Sets up the sound
+        let alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(noteList[count], ofType: "wav")!)
+        print(alertSound)
+        
+        // Removed deprecated use of AVAudioSessionDelegate protocol
+        //var errors:NSError?
+        do
+        {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: alertSound)
+            
+            
+        }
+        catch _ { }
+        
+        beingPlayed = 1; //indicates the note is being played
+        audioPlayer.prepareToPlay()
+        audioPlayer.numberOfLoops = -1 //for infinite holding
+        audioPlayer.play()
+        print(count)
+
     }
     
     @IBAction func DownButton(sender: AnyObject) {
@@ -39,35 +75,26 @@ class Tuner: UIViewController {
         }
         print(count)
         Note.text = noteNames[count]
+        if beingPlayed == 1 {
+            playNote()
+        }
     }
     
     @IBOutlet var Note: UILabel!
+    
     //play button: plays sound
     @IBAction func play(sender: AnyObject) {
-        
-        //Sets up the sound
-        let alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(noteList[count], ofType: "wav")!)
-        print(alertSound)
-        
-        // Removed deprecated use of AVAudioSessionDelegate protocol
-        //var errors:NSError?
-        do
-        {
-            
-            try audioPlayer = AVAudioPlayer(contentsOfURL: alertSound)
-        }
-        catch _ { }
-        
-        audioPlayer.prepareToPlay()
-        audioPlayer.play()
-        print(count)
-        
+       playNote()
     }
     
-
     //stops the music
     @IBAction func stopButton(sender: AnyObject) {
+        stopNote()
+    }
+    
+    func stopNote(){
         audioPlayer.stop()
+        beingPlayed = 0;
     }
     
     @IBOutlet var NotePlayed: UIButton!
@@ -76,12 +103,14 @@ class Tuner: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //initializes all the note files and the displayed note names
         noteList = ["0A", "1As", "2B", "3C", "4Cs", "5D", "6Ds", "7E", "8F", "9Fs", "10G", "11Gs"]
-        
         noteNames = ["A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#"]
         
+        //changes the note display
         Note.text = noteNames[count]
         
+        //for the swipe
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
 }
