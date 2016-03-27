2dc13d4a1bb8df083440d62d0d7f5a69d153017a
youngliu323 <youngliu323@gmail.com>
2016-03-27 15:48:49 -0500

    facebook login

--- FermataBeta/FermataBeta/Login.swift
+++ FermataBeta/FermataBeta/Login.swift
@@ -7,11 +7,46 @@
 //
 
 import Foundation
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class Login: UIViewController {
+class Login: UIViewController, FBSDKLoginButtonDelegate {
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        if (FBSDKAccessToken.currentAccessToken() == nil){
+            print("not logged in")
+        } else {
+            print("logged in!")
+        }
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error == nil{
+            print("login complete")
+            //self.performSegueWithIdentifier("facebookConf", sender: self)
+        } else {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User logged out")
     }
     
 }
