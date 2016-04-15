d641c65720376258fa40081dcd52b8971be30a50
kyleroush <roush.kyle@gmail.com>
2016-04-15 14:49:43 -0500

    Added files via upload

---
+++ ajax.js
@@ -0,0 +1,43 @@
+function loadsong(songname)
+{
+    console.log(songname);
+    //$.get('http://people.eecs.ku.edu/~sbenson/grabMidi.php',{
+          //title: songname
+          $.get('http://people.eecs.ku.edu/~sxiao/grabMidi.php',{
+          id: songname
+
+},
+function(data, status){
+console.log(status)
+console.log(data)
+return parseMidi(data)
+});
+}
+
+
+function loadsongs(songname)
+{
+console.log(songname);
+
+$.get('http://people.eecs.ku.edu/~sxiao/grabTitles.php',{
+//$.get('http://people.eecs.ku.edu/~sbenson/grabTitles.php',{
+      },
+      function(data, status)
+{
+console.log(status)
+console.log(data)
+var songs=data.split('@')
+songs.pop()//remove empty one at end
+console.log(songs);
+for(num in songs){
+$("#songlist")[0].innerHTML+="<li onclick= songclicked('"+songs[num]+"')>"+songs[num]+"</li>"
+}
+//return parseMidi(data)
+      });
+}
+function songclicked(string) {
+$("#songlist")[0].innerHTML=""
+loadsong(string)
+
+// body...
+}
