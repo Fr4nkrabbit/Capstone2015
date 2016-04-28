3f9069c0f0cb3ad551af5954323169b9f42a604d
kyleroush <roush.kyle@gmail.com>
2016-04-28 02:11:07 -0500

    Update parser.js

--- midiToVex/parser.js
+++ midiToVex/parser.js
@@ -1,354 +1,425 @@
-function durToWhole(inFloat) //.125 > 8
+function fromFile()
 {
-	return parseInt(1 / inFloat)
-}
+	//plzWait();
+	//fName(document.getElementById('file').value);
 
-function pitToStr(inHex,clef) //5C > G#6
-{
-	if (inHex == -1&&clef=="treble")
-		return 'B/4' //rest position
-	else if (inHex == -1&&clef=="bass")
-		return 'd/3' //rest position
-
-
-	var note = Vex.Flow.integerToNote(parseInt(inHex, 16) % 12)
-	var octave = parseInt(parseInt(inHex, 16) / 12) - 1
-
-	return note + '/' + octave
-}
-
-function quantizeNote(inDec) //0.5598958333333334 => 0.5625
-{
-	var step = 1 / 128
-	var skew = inDec % step
-
-	if (skew)
+	if(window.FileReader)
 	{
-		inDec -= skew
+		var reader=new FileReader();
+		var f=document.getElementById('file').files[0];
+		reader.onload=function(e){
+			//console.log(e.target.result)
 
-		if (skew > step / 2) //round up?
-			inDec += step
+			parseMidi(e.target.result);};
+
+		reader.readAsBinaryString(f);
 	}
-
-	return inDec
+	else err('File API is not supported in this browser!');
 }
 
-function generateStaveNotes(measure,keysig,clef){
 
+var parsedEvents
 
-
-	var notes = []
-
-	var forbeamnotes=[],
-	allbeams=[],
-	lengthofbeam=0;
-
-	var tuplets = []
-	var tripletCount = 0 //count to 3, then will create a triplet object
-
-	for (var n = 0, i=0; n < measure.length; n++) //loop through notes
-	{
-		//console.log(inM[mC].m[n])
-		var decDur = measure[n].note
-		var pit = pitToStr(measure[n].pitch,clef)
-		var dur = durToWhole(decDur)
-		var dotted = false
-		var irregularNote = true
-		var tosmall = true
-
-		//Determine if the note is at least normal or dotted
-		//for (var p = 0; p < 8 && irregularNote; p++)
-		for (var p = 0; p < 7 && irregularNote; p++) //p = power of 2 to check against
-		{
-			if (measure[n].note * 2 / 3 == 1 / Math.pow(2,p)) //regular dotted
-			{
-				dur = durToWhole((2/3)*(decDur))
-				dur = dur + 'd'
-				dotted = true
-				irregularNote = false
-			}
-			else if (decDur == 1 / Math.pow(2,p)) //regular
-			{
-				irregularNote = false
-			}
-			else if (decDur == 1 / (3 * Math.pow(2,p))) //regular triplet
-			{
-				dur = Math.pow(2, p+1)
-				tripletCount++
-				irregularNote = false
-			}
-		}
-
-		//irregular, triplet or regular
-
-		if (irregularNote) //Create a collection of mini-notes to sum to the irregular note
-		{
-			//console.log('irregular note')
-			decDur = quantizeNote(decDur) //round the duration to the nearest 1/128th
-
-			var collection = [] //holds integers of the notes to be added
-
-			for (var t = 0; t < 8 && decDur > 0; t++)
-			{
-				while (decDur >= 1 / Math.pow(2,t))
-				{
-					collection.push(Math.pow(2,t))
-					decDur -= 1 / Math.pow(2,t)
-				}
-			}
-
-			//push the notes
-			for (var col = 0; col < collection.length; col++)
-				notes.push(new Vex.Flow.StaveNote({clef:clef,  keys: [pit], duration: collection[col].toString()}))
-		}
-		else if (tripletCount > 0)
-		{
-			//console.log('triplet note')
-
-			//console.log('pit ' + pit)
-			//console.log('dur ' + dur)
-			dur += ''
-			notes.push(new Vex.Flow.StaveNote({ clef:clef, keys: [pit], duration: dur }))
-
-			if (tripletCount == 3)
-			{
-				//console.log('just read 3 triplets')
-				tripletSet = notes.slice(notes.length-3, notes.length)
-
-				tuplets.push(new Vex.Flow.Tuplet(
-					tripletSet,
-					{num_notes: 3,
-					notes_occupied: 2}))
-				tripletCount = 0
-			}
-		}
-		else //Handle this note normally
-		{
-			//console.log('normal note')
-			dur = dur + (measure[n].pitch == -1 ? 'r' : '') //append 'r' if this note is a rest
-
-			//console.log('note' + n + ': ' + dur + ', ' + pit + ', dotted ' + dotted + " + " + inM[mC].m[n].pitch)
-
-			//console.log('pit ' + pit)
-			//console.log('dur ' + dur)
-			notes.push(new Vex.Flow.StaveNote({clef:clef, keys: [pit], duration: dur }))
-
-			if(dotted)
-				notes[notes.length-1].addDotToAll()
-
-			if(!keysig.acidentals.includes(pit) && pit.includes('#'))
-				notes[notes.length-1].addAccidental(0, new Vex.Flow.Accidental("#"))
-		}
-	} //end looping through notes
-	//console.log('')
-return [notes,tuplets]
-
-
-}
-
-function create_voice(sig)
+function stringParseMidi(s)
 {
-	return new Vex.Flow.Voice(
-	{
-		num_beats:	sig.top,
-		beat_value:	sig.bottem,
-		resolution:	Vex.Flow.RESOLUTION
-	}).setMode(2)
+	var jzzmf=JZZ.MidiFile.fromBase64(s)
+	parseMidi(jzzmf)
 }
 
-
-function drawSheets(inM)
+function parseMidi(s)
 {
-/*
-for each line create a new struct that will have each instruments list of measures
-for that line
 
-*/
-	console.log("inM")
-	console.log(inM)
-	var canvas = document.querySelector("canvas")
-	var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS)
-	var ctx = renderer.getContext()
 
-	var timesig =
+	var mf = new JZZ.MidiFile(s)
+
+	//console.log(mf)
+	var trans=translateMfEvents(mf);
+	//console.log(trans)
+	var events =gatherEvents(trans)
+	//console.log(events)
+	var instruments=[]
+
+	for (instrument in events.instruments)
+		instruments.push(instrument)
+
+	console.log('instrument list: ')
+	console.log(instruments)
+
+	parsedEvents = events
+
+	if(instruments.length==1)
+		createSheetMusic([instruments[0]])
+	else
 	{
-		top: 4,
-		bottem: 4
-	};
-	timesig=inM[Object.keys(inM)[0]].measures[0].time
-
-	var keysig =
-	{
-		name: "C",
-		acidentals:[]
+		instlister()
+		return instruments
 	}
-var instrnum=0
-	var stoppingPoint = Math.min(inM.length, (LINE_COUNT+STARTING_MEASURE)* MEASURES_PER_LINE)
+}
 
-	for(var line =0;((line+1)*Object.keys(inM).length)<LINE_COUNT;line++){
-		//console.log("line "+line)
+function createSheetMusic(instruments)
+{
+	var m=[]
+	if(parsedEvents==undefined)
+		return "no events"
+	for(i in instruments){
+		var notesToShow = combineNotes(parsedEvents.instruments[instruments[i]], parsedEvents.ppqn)
+
+		//fix overlapping notes
+		notesToShow = forceMonophonic(notesToShow, parsedEvents.ppqn)
+
+		//add in rests where there are gaps
+		notesToShow = createRest(notesToShow, parsedEvents.ppqn,parsedEvents.timesig)
+
+		//console.log(notesToShow)
+		var measures=populateMeasures(notesToShow,parsedEvents.ppqn,parsedEvents.keysig,parsedEvents.timesig)
+
+		console.log(measures)
+		parsedEvents[instruments[i]]=measures
+		m[instruments[i]]={measures: measures, clef: CLEF}
+
+	}
+	TO_DRAW=m
+	drawSheets(m)
+}
+
+        //takes a midi file that has been parsed into a 64 bit string and generates sheet music
+function readMidiFile(s)
+{
+	//console.log(s)
+	try
+	{
+		var mf = new JZZ.MidiFile(s)
+
+		//console.log(mf)
+		var outMeasures = displayMidiFile(mf)
+
+		drawSheets(outMeasures)
+	}
+	catch(e){ err(e) }
+}
 
 
-		for (var mC = 0; mC < MEASURES_PER_LINE; mC++) //loop through measures
+//this takes in a list of midi tracks and there events and returns a list of all the events in string format
+var globalppqn;
+function translateMfEvents(mf)
+{
+	globalppqn=mf.ppqn
+	var arg =[];
+
+	for (var i = 0; i < mf.length; i++)
+	{
+		if (mf[i] instanceof JZZ.MidiFile.MTrk)
 		{
-		//	console.log("meaure "+mC)
-			instrnum=0
-			var measNum=mC+line*MEASURES_PER_LINE+STARTING_MEASURE
-			var staves=[]
-
-			for (var instr in inM){//for the different instruments
-				//console.log("instr "+instr);
-				//Decide where to draw the measure, or "staff" as Vexflow calls it
-				var offsetX = STAFF_OFFSET_X + mC * STAFF_WIDTH
-				var offsetY = STAFF_OFFSET_Y * ((line*Object.keys(inM).length)+instrnum)
-				var stave = new Vex.Flow.Stave(offsetX, offsetY, STAFF_WIDTH)
-				var shift=60
-
-				staves.push(stave)
-				instrnum++
-				// Add a treble clef if it's the first measure of the line
-				if (mC % MEASURES_PER_LINE == 0){
-					stave.addClef(inM[instr].clef)
-					shift+=50
-				}
-
-				if(inM[instr].measures[measNum].time)
-				{
-					stave.addTimeSignature(""+timesig.top+'/'+timesig.bottem)
-					timesig =inM[instr].measures[measNum].time
-				}else if (mC % MEASURES_PER_LINE == 0){
-					stave.addTimeSignature(""+timesig.top+'/'+timesig.bottem)
-
-				}else{
-					shift-=30
-				}
-
-				if(inM[instr].measures[measNum].key!=undefined)
-				{
-					keysig.name =inM[instr].measures[measNum].key
-					keysig.Accidental=[]
-					stave.addKeySignature(keysig.name)
-				}else if (mC % MEASURES_PER_LINE == 0){
-					stave.addKeySignature(keysig.name)
-
-				}else{
-					shift-=30
-				}
-
-				//stave.addKeySignature(keysig.name)
-				stave.setContext(ctx).draw()
-
-				//create notes from inM
-
-				var notesAndTuplets=generateStaveNotes(inM[instr].measures[measNum].m,keysig,inM[instr].clef)
-				var notes =notesAndTuplets[0]
-				var tuplets =notesAndTuplets[1]
-
-				//Add beams
-				allbeams = Vex.Flow.Beam.generateBeams(notes)
-
-				// Create a voice in 4/4
-
-				// Create voices and add notes to each of them
-				var voice = create_voice(timesig).addTickables(notes)
-
-
-				// Format and justify the notes
-
-				var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], STAFF_WIDTH-shift)
-
-				// Render voice
-				voice.draw(ctx, stave)
-
-				// Render beams
-				while(allbeams.length)
-					allbeams.pop().setContext(ctx).draw()
-
-				// Render tuplets
-				while(tuplets.length)
-					tuplets.pop().setContext(ctx).draw()
-
-			} //end looping through measures
-			if(Object.keys(inM).length!=1){
-				new Vex.Flow.StaveConnector(staves[0],staves[staves.length-1]).setType(1).setContext(ctx).draw()
-
-				if(mC==0){
-					new Vex.Flow.StaveConnector(staves[0],staves[staves.length-1]).setType(3).setContext(ctx).draw()
-				}else if(mC+1==MEASURES_PER_LINE){
-					new Vex.Flow.StaveConnector(staves[0],staves[staves.length-1]).setType(0).setContext(ctx).draw()
-
-				}
+			for (var j = 0; j < mf[i].length; j++)
+			{
+				var evt = mf[i][j]
+				var s = evt.toString().replace(/&/g,'&amp;').replace(/>/g,'&gt;').replace(/</g,'&lt;')
+				arg.push(evt.time + " " + s)
 			}
-
 		}
 	}
-	console.log("Successful run!")
-    currentMeasures()
-
+	return arg;
 }
 
-function startBasicFollow(tempo){
-    FOLLOW_TEMPO=tempo/60*1000
-    follow()
+function gatherEvents(arg)
+{
+	var instruments = {}
+	var events = {}
 
+	events.notes = []
+	events.tempo = []
+	events.keysig = []
+	events.timesig = []
+	events.ppqn = globalppqn
 
+	var lasttime = 0;
+	var hitnote = false;
+	var title;
+	var notes = [];
+
+	for (item in arg)
+	{
+		var lts = arg[item].split(' ');
+
+		//console.log('lts:')
+		//console.log(lts)
+
+		if(lasttime>parseInt(lts[0])&&hitnote)
+		{
+			//console.log('assigning notes to an instrument')
+			//console.log(title)
+
+			console.log('title is '); console.log(title)
+			var pendingName = "invalid title given"
+			if (title)
+				pendingName = title.splice(4).join(" ")
+
+			instruments[pendingName] = events.notes;
+			events.notes=[]
+			hitnote=false
+		}
+
+		lasttime=parseInt(lts[0]);
+
+		if(parseInt(lts[1]/10) == 9)
+		{
+			events.notes.push(lts)
+			hitnote=true
+		}
+
+		if(parseInt(lts[1]/10) == 8)	events.notes.push(lts)
+		if(lts[1] == "ff51")			events.tempo.push(lts)
+		if(lts[1] == "ff59")			events.keysig.push(lts)
+		if(lts[1] == "ff58")			events.timesig.push(lts)
+
+		if(lts[1] == "ff03")
+		{
+			title=lts
+			//console.log('saw ff03')
+			//console.log(title)
+		}
+	}
+
+	if(hitnote)
+	{
+		console.log('title is '); console.log(title)
+
+		var pendingName = "invalid title given"
+		if (title) pendingName = title.splice(4).join(" ")
+
+		instruments[pendingName] = events.notes;
+		//events.notes=[]
+		hitnote=false
+	}
+
+	events.instruments=instruments
+	return events
 }
 
-function follow(){
-    timer=setTimeout(follow,FOLLOW_TEMPO*MEASURES_PER_LINE)
-    STARTING_MEASURE++
-    $('canvas').parent()[0].innerHTML='<canvas width=' + (CANVAS_WIDTH+25) + ' height=' + CANVAS_HEIGHT + '></canvas>'
-		drawSheets(TO_DRAW)
+//this needs to be handed a list of that contains the starts and stops of notes and also the pulse per quater note
+function combineNotes(notes,ppqn)
+{
+	var combined=[]
+	//go though and combine the note start and stops
+	for (item = 0; item < notes.length; item++)
+	{
+		if (notes[item][6] == "On")
+		{
+			for (thing = item+1; thing < notes.length; thing++)
+			{
+				if (notes[item][2] == notes[thing][2])
+				{
+					combined.push({
+						start:	parseInt(notes[item][0]),
+						finish:	parseInt(notes[thing][0]),
+						pitch:	notes[item][2],
+						note:	((notes[thing][0]-notes[item][0])/ppqn)/4
+						})
+					break
+				}
+			}
+		}
+	}
 
-    //currentMeasures()
+	return combined
 }
 
-function turnPage(dir){
-    STARTING_MEASURE+= LINE_COUNT*MEASURES_PER_LINE*dir
-    $('canvas').parent()[0].innerHTML='<canvas width=' + (CANVAS_WIDTH+25) + ' height=' + CANVAS_HEIGHT + '></canvas>'
+//resize notes that overlap
+function forceMonophonic(notes, ppqn)
+{
+	var lastTime = 0
 
-		drawSheets(TO_DRAW)
-    //currentMeasures()
+	//console.log('notes: ')
+	for (n in notes)
+	{
+		//console.log(notes[n])
+		if (lastTime > notes[n].start)
+		{
+			///console.log(lastTime + ' vs ' + notes[n].start)
+			notes[n-1].finish = notes[n].start //truncate the previous note.finish to end when this note starts
+			notes[n-1].note = ((notes[n-1].finish - notes[n-1].start) / ppqn) / 4
+		}
+
+		lastTime = notes[n].finish
+	}
+
+	//console.log(notes)
+
+	return notes
 }
 
-function singleMeasure(dir ){
-    STARTING_MEASURE+= dir
-    $('canvas').parent()[0].innerHTML='<canvas width=' + (CANVAS_WIDTH+25) + ' height=' + CANVAS_HEIGHT + '></canvas>'
+//fills in the gap between notes with rest
+function createRest(notes,ppqn,timesig)
+{
+	console.log('inside createRest ' + ppqn*4)
+	console.log(notes)
+	var lasttime=0
+	var notesWithRest=[]
 
-		drawSheets(TO_DRAW)
+	for (note in notes)
+	{
+		//checks to see if the last notes ended when the current note starts
+		//to check to see if we need to add something to fill in that gap
+		//I used greater that 10 because i noticed that would be a gap of
+		// 1 sometimes between notes so i figured we could just use a buffer
+		//to make sure we dont create unnescarry rest
+		//console.log('note starts at ' + notes[note].start)
+
+		var noteDec = ((notes[note].start- lasttime)/ppqn) / 4
+		//if (noteDec == 0) console.log('createRest is trying to push 0')
+
+		//if (lasttime - notes[note].start <= -10)
+		if (notes[note].start - lasttime > 0 && noteDec > 0)
+		{
+			//console.log ('gap of size ' + (notes[note].start - lasttime))
+			notesWithRest.push({
+			start:	lasttime,
+			finish:	notes[note].start,
+			pitch:	-1,
+			note:	noteDec
+			})
+		}
+
+		notesWithRest.push(notes[note])
+		lasttime=notes[note].finish
+
+		//console.log('lasttime is now ' + lasttime)
+	}
+
+	//  var trailingRest = lasttime % (ppqn * 4)
+	var lastmeas = timesig[timesig.length-1]
+	var measureSize = (4/lastmeas[4].split('/')[0]) * ppqn * lastmeas[4].split('/')[0]
+	var trailingRest = lasttime % measureSize
+
+	//console.log('lasttime ' + lasttime + ' and whole note ' + (ppqn * 4))
+
+	if (trailingRest > 0)
+	{
+		var startT = lasttime
+		var finishT = lasttime + ((ppqn * 4) - trailingRest)
+
+		//if the last note didn't end the measure, insert a rest
+		notesWithRest.push({
+			start:	startT,
+			finish:	finishT,
+			pitch:	-1,
+			note:	((finishT - startT)/ppqn)/4
+			})
+
+			//console.log('trailing rest:')
+			//console.log(notesWithRest[notesWithRest.length-1])
+	}
+
+	return notesWithRest
 }
 
-function singleLine(dir){
-    STARTING_MEASURE+= dir*MEASURES_PER_LINE
-    $('canvas').parent()[0].innerHTML='<canvas width=' + (CANVAS_WIDTH+25) + ' height=' + CANVAS_HEIGHT + '></canvas>'
-
-		drawSheets(TO_DRAW)
-
-}
-function goToMeasure(meas){
-    STARTING_MEASURE=meas
-    $('canvas').parent()[0].innerHTML='<canvas width=' + (CANVAS_WIDTH+25) + ' height=' + CANVAS_HEIGHT + '></canvas>'
-
-		drawSheets(TO_DRAW)
-
-}
-
-function setMeasurePerLine(num){
-    MEASURES_PER_LINE=num
-    $('canvas').parent()[0].innerHTML='<canvas width=' + (CANVAS_WIDTH+25) + ' height=' + CANVAS_HEIGHT + '></canvas>'
-
-		drawSheets(TO_DRAW)
-}
-function setLineCount(num){
-    LINE_COUNT=num
-    $('canvas').parent()[0].innerHTML='<canvas width=' + (CANVAS_WIDTH+25) + ' height=' + CANVAS_HEIGHT + '></canvas>'
-
-		drawSheets(TO_DRAW)
-}
 
 
-function stopTimer(){
-    clearTimeout(timer)
+//this will take the list of notes and the time signature and then create list
+//of notes that fit into measures
+function populateMeasures(notes2,ppqn,keysig,timesig)
+{
+
+	var averageNotePitch=0
+	var counter=0
+
+	var laststart = -1
+	var lastfinish = 0
+	console.log(ppqn)
+	// initial measure; added in to create the initial time measure in order to correctly create the the measures -ER
+	var initMeas = {m: []}
+	var lastMeas
+	var holder
+	var measures = []
+	var meas
 
 
+
+	function newMeas()
+	{
+		meas = {m: []}
+
+		if(keysig[0]&&lastfinish==parseInt(keysig[0][0]))
+		{
+			meas.key=keysig[0][4]
+			keysig=keysig.slice(1)
+		}
+
+		if(timesig[0]&&lastfinish==parseInt(timesig[0][0]))
+		{
+			meas.time =
+			{
+				top:	timesig[0][4].split('/')[0],
+				bottem:	timesig[0][4].split('/')[1]
+			}
+			lastMeas =
+			{
+				top:	timesig[0][4].split('/')[0],
+				bottom:	timesig[0][4].split('/')[1]
+			}
+
+			timesig=timesig.slice(1)
+		}
+	}
+
+	newMeas()
+	var ppqmeas = (lastMeas.top/lastMeas.bottom)*ppqn*4 // running counter of the ending pulse of the currently read measures
+
+	//populate measures
+	for (noter in notes2)
+	{
+		if(parseInt(notes2[noter].pitch,16)!=-1){
+			averageNotePitch+=parseInt(notes2[noter].pitch,16)
+			counter++
+		}
+		if(notes2[noter].start >= ppqmeas)
+		{
+			//adds a measures if the note we are about to look at is in another measure
+			//console.log(notes2[noter].start)
+			//console.log(ppqmeas)
+			//console.log("push a measure")
+			measures.push(meas)
+			newMeas()
+			//console.log(lastMeas.bottom)
+			//console.log(lastMeas.top)
+			ppqmeas += (lastMeas.top/lastMeas.bottom)*ppqn*4
+		}
+
+
+		if (notes2[noter].finish > ppqmeas)
+		{
+			//for notes that go over a measure and need to be split into notes for each indivdual measure
+			while (notes2[noter].finish > ppqmeas)
+			{
+				meas.m.push({
+				start:	notes2[noter].start,
+				finish:	ppqmeas,
+				pitch:	notes2[noter].pitch,
+				note:	((ppqmeas-notes2[noter].start) / ppqn)/4,
+				tie: true
+				})
+				notes2[noter].start = ppqmeas
+				notes2[noter].note = ((  notes2[noter].finish-notes2[noter].start)/ppqn)/4
+
+				//console.log(ppqmeas)
+				measures.push(meas)
+
+				newMeas()
+				ppqmeas += (lastMeas.top/lastMeas.bottom)*ppqn*4
+
+				notes2[noter].tie=true
+			}
+		}
+
+		//add note
+		meas.m.push(notes2[noter])
+		lastfinish = notes2[noter].finish
+	}
+
+	//console.log("pushing last measure")
+	measures.push(meas)
+	averageNotePitch/=counter
+	if(averageNotePitch<60)
+		CLEF="bass"
+	return measures
 }
