4fda0edddfec60cc4f0f66f5c58298017e034619
kyleroush <roush.kyle@gmail.com>
2016-04-28 02:06:14 -0500

    Update instruments.js

--- midiToVex/instruments.js
+++ midiToVex/instruments.js
@@ -5,10 +5,11 @@ for(num in parsedEvents.instruments)
 {
 
 //wrap "s around the identifying name
-var identifyingName = '"' + num + '"'
-var displayName = num
-
-$("#instrumentlist")[0].innerHTML += '<input type=radio name=instrument value='+identifyingName+'>'+displayName+'<br>'
+var displayName = num.split(" ").join("")
+var identifyingName = '"' + displayName + '"'
+console.log(num)
+//$("#instrumentlist")[0].innerHTML += '<input type=checkbox name=instrument value='+identifyingName+'>'+displayName+'<br>'
+$("#instrumentlist")[0].innerHTML +="<div id='"+displayName+"' class='uncheckbox' value='"+num+"' onclick=checker("+identifyingName+")><h1>"+num+"</h1></div>"
 
 console.log($("#instrumentlist")[0].innerHTML)
 }
@@ -17,8 +18,10 @@ $("#instrumentlist")[0].innerHTML+="<input type=button onclick= instclicked() va
 function instclicked() {
 
 //document.querySelector("#tester1").innerText="string"
-string=document.querySelector('input:checked').value
+string=[]//=document.querySelector('input:checked').value
 //document.querySelector("#tester1").innerText=string
+for( var i =0;i< $('.checkbox').length;i++)
+  string.push($('.checkbox')[i].getAttribute("value"))
 document.querySelector("#instrumentlist").innerHTML=""
 
 //
@@ -29,9 +32,29 @@ createSheetMusic(string)
 // body...
 }
 function getInstruments() {
-var instruments=[]
+  var instruments=[]
 
-for (instrument in parsedEvents.instruments)
-instruments.push(instrument)
-return instruments.toString()
+  for (instrument in parsedEvents.instruments)
+  instruments.push(instrument)
+  return instruments.toString()
+}
+
+
+
+function style(){
+  var instr = ["flute","oboe","drums","voclal"]
+  for(i in instr)
+  $("#instrumentlist")[0].innerHTML +='<div id="'+instr[i]+'" class="uncheckbox" onclick=checker("'+instr[i]+'")><h1>'+instr[i]+'</h1></div>'
+
+
+}
+
+var allvals=[]
+function checker(val){
+console.log(val)
+$('#'+val).toggleClass('uncheckbox');
+
+$('#'+val).toggleClass('checkbox');
+
+
 }
